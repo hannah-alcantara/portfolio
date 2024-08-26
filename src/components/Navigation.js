351353a6a5aacf53067ecadecf98fd3bede866ac
hannah <hannah.alcantara.586@my.csun.edu>
2024-08-25 21:45:37 -0700

    add: components for home page

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <nav className='p-8 flex justify-center gap-5'>
+        <a href="">Work</a>
+        <a href="">About</a>
+        <a href="">Contacts</a>
+    </nav>
+  )
+}
+
+export default Navigation
